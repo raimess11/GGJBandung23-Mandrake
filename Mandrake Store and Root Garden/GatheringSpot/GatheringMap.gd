8bd6455f5af667e1e80ea656f31ec5be64c7e23b
Fakhrul0121 <106474317+Fakhrul0121@users.noreply.github.com>
2023-02-04 19:35:05 +0700

    gathering spot

---
+++ Mandrake Store and Root Garden/GatheringSpot/GatheringMap.gd
@@ -0,0 +1,42 @@
+extends CanvasLayer
+
+onready var reminderUI = $reminderUI
+onready var title = $reminderUI/ColorRect/title
+onready var desc = $reminderUI/ColorRect/description
+onready var fadePause = $fadePause
+
+var gatheringSpotToGo = ""
+
+func disableBackground():
+	pass
+
+func _on_swamp_pressed():
+	get_tree().paused = true
+	fadePause.visible = true
+	reminderUI.visible = true
+	title.text = "swamp"
+	desc.text = "a 2 days journey with a 7n coins bargain"
+
+
+func _on_forest_pressed():
+	get_tree().paused = true
+	fadePause.visible = true
+	reminderUI.visible = true
+	title.text = "forest"
+	desc.text = "a 2 days journey with a 6n coins bargain"
+
+
+func _on_river_pressed():
+	get_tree().paused = true
+	fadePause.visible = true
+	reminderUI.visible = true
+	title.text = "river"
+	desc.text = "a 1 day journey with a 2n coins bargain"
+
+
+func _on_mountain_pressed():
+	get_tree().paused = true
+	fadePause.visible = true
+	reminderUI.visible = true
+	title.text = "mountain"
+	desc.text = "a 3 days journey with a 12n coins bargain"
