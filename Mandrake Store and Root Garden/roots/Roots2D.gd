b6b19a2d4bababc97cb12b84be5dc7381e26122c
raimess11 <rahma.sakti24@gmail.com>
2023-02-05 03:10:39 +0700

    im stuck here, push for check point

--- Mandrake Store and Root Garden/roots/Roots2D.gd
+++ Mandrake Store and Root Garden/roots/Roots2D.gd
@@ -1,4 +1,15 @@
 extends Node2D
 
+export(Resource) var data = preload("res://roots/Ríza Dilitiríou.tres")
 
-extends(Resource) data = ""
+var selected:bool = true
+var onDropPoint:bool = false
+
+func _ready():
+	$Sprite.texture = data.texture
+
+func _process(delta):
+	if selected:
+		global_position = get_global_mouse_position()
+	else:
+		onDropPoint=true
