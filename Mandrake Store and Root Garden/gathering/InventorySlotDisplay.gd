0f855fdfdce9aed107bc45a1eae16f27ccda7745
raimess11 <rahma.sakti24@gmail.com>
2023-02-04 19:01:43 +0700

    testing program with Riza Dilitirious
    mengimplementasikan texture ke program dan testing inventory slot

--- Mandrake Store and Root Garden/gathering/InventorySlotDisplay.gd
+++ Mandrake Store and Root Garden/gathering/InventorySlotDisplay.gd
@@ -1,9 +1,14 @@
 extends CenterContainer
 
 onready var itemTextureRect = $TextureRect
+onready var itemQuantityLabel = $Label
 
 func display_roots(roots):
+	
 	if roots != null:
-		itemTextureRect.texture = roots.Texture
+		itemTextureRect.texture = roots.texture
+		var quantity = roots.quantity
+		itemQuantityLabel.text = str(quantity) if quantity > 0 else ""
+		#itemTextureRect.self_modulate.a = int(roots.quantity == 0)
 	else:
 		pass
