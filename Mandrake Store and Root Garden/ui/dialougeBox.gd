912cc0e7fd7a1fd1cf4999fde6a0257816f3f1a3
Fakhrul0121 <106474317+Fakhrul0121@users.noreply.github.com>
2023-02-04 22:19:55 +0700

    dialougebox

---
+++ Mandrake Store and Root Garden/ui/dialougeBox.gd
@@ -0,0 +1,13 @@
+extends TextureRect
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_Accept_pressed():
+	pass # Replace with function body.
+
+
+func _on_Decline_pressed():
+	pass # Replace with function body.
